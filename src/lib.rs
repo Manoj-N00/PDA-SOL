d91889f062078c3825713783558e90498a7e7219
Manoj-N00 <manojnaikj@gmail.com>
2025-06-15 23:11:55 +0530

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,15 @@
-use solana_program::{account_info::AccountInfo,Pubkey};
+use solana_program::{account_info::{next_account_info ,AccountInfo},entrypoint::{self ,ProgramResult}, pubkey::Pubkey};
+
+entrypoint!(process_instruction);
 
 
-fn process_instruction(
-    public_key:&Pubkey,
-    accoumts:&[AccountInfo],
-
-)
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let iter = &mut accounts.iter();
+    let pda = next_account_info(iter);
+    let user_acc = next_account_info(iter);
+    let sysyem_program= next_account_info(iter);
+}
