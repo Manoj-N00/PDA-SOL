51eef72483dfe818e46248c56bf98769c9acf3db
Manoj-N00 <manojnaikj@gmail.com>
2025-06-15 23:43:26 +0530

    ix

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-use solana_program::{account_info::{next_account_info ,AccountInfo},entrypoint::{self ,ProgramResult}, pubkey::Pubkey};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint::{self, ProgramResult},
+    program::invoke_signed,
+    pubkey::Pubkey,
+    system_instruction::create_account,
+};
 
 entrypoint!(process_instruction);
-
 
 pub fn process_instruction(
     program_id: &Pubkey,
@@ -9,7 +14,11 @@ pub fn process_instruction(
     instruction_data: &[u8],
 ) -> ProgramResult {
     let iter = &mut accounts.iter();
-    let pda = next_account_info(iter);
-    let user_acc = next_account_info(iter);
-    let sysyem_program= next_account_info(iter);
+    let pda = next_account_info(iter)?;
+    let user_acc = next_account_info(iter)?;
+    let sysyem_program = next_account_info(iter)?;
+
+    let ix = create_account(user_acc.key, pda.key, 1000000000, 8, program_id);
+
+    invoke_signed(ix)
 }
